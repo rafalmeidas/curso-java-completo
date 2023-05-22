cfbd3db1c815b0dee53df1f6e7739aca268e41d7
Rafael Silva <rafalmeidas21@gmail.com>
2023-05-22 20:18:03 -0300

    Primeiras aulas com código!

---
+++ src/fundamentos/DesafioTemperatura.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class DesafioTemperatura {
+    public static void main(String[] args) {
+        final double MULTIPLIER = 5 / 9.0;
+        final int ADJUSTMENT = 32;
+        double fahrenheit = 75;
+        double celsius = (fahrenheit - ADJUSTMENT) * MULTIPLIER;
+
+        System.out.println(fahrenheit+"ºF = "+ celsius+"ºC");
+    }
+}
---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,8 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro programa #01");
+        System.out.println("Meu primeiro programa #02");
+    }
+}
